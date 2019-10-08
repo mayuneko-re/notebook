7cd00c48e3dc8f01996b98f7ccc4063379c087f3
mayuneko-re <49189086+mayuneko-re@users.noreply.github.com>
2019-10-08 14:45:15 +0900

    Update buckley_leverett.py

--- buckley_leverett.py
+++ buckley_leverett.py
@@ -5,6 +5,37 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 
 class BL:
+    """Store and calculate data for oil-water displacement problem by Buckley-Leverett Solution
+    
+    Attributes:
+        muw (float): Water viscosity, cP
+        muo (float): Oil viscosity, cP
+        Swc (float): Connate water saturation, Vol/Vol
+        Sor (float): Residual oil saturation, Vol/Vol
+        krw0 (float): Water endpoint relative permeability at residual oil saturation
+        kro0 (float): Oil endpoint relative permeability at connate water saturation
+        nw (float): Exponent for water relative permeability
+        no (float): Exponent for oil relative permeability
+        label (str): Case label for plot
+        Sw (ndarray): Water saturation
+        Swn (ndarray): Normalized water saturation
+        krw (ndarray): Water relative permeability
+        kro (ndarray): Oil relative permeability
+        fw (ndarray): Fractional flow of water
+        vD (ndarray): Wave velocity
+        vD_sf (float): Shock Front velocity
+        Sw_sf (float): Shock Front saturation
+        fw_wSF (ndarray): Fractional flow of water with Shock Front
+        tD (ndarray): Dimensionless time for plot
+        tD_BT (float): Dimensionless time at breakthrough
+        Sw_out (ndarray): Water saturation at outlet
+        fw_out (ndarray): Fractional flow of water at outlet
+        N (ndarray): Oil production volume, PV
+        N_BT (float): Oil production volume at breakthrough, PV
+        RF (ndarray): Oil recovery factor, HCPV
+        RF_BT (float): Oil recovery factor, HCPV
+    """
+    
     def __init__(self, muw, muo, Swc, Sor, krw0, kro0, nw, no, label=None):
 
         self.muw = muw # cP, Water viscosity
@@ -15,7 +46,7 @@ class BL:
         self.kro0 = kro0 # Oil endpoint relative permeability at connate water saturation
         self.nw = nw # Exponent for water relative permeability
         self.no = no # Exponent for oil relative permeability
-        self.label = label
+        self.label = label # Label for plot
 
     def calc(self):
 
@@ -23,8 +54,8 @@ class BL:
         self.Sw = np.linspace(start=self.Swc, stop=1-self.Sor, num = 300)
         self.Swn = (self.Sw-self.Swc) / (1-self.Swc-self.Sor)
 
-        self.krw = self.relperm(Sn=self.Swn, kr0=self.krw0, n=self.nw) # Water
-        self.kro = self.relperm(Sn=1-self.Swn, kr0=self.kro0, n=self.no) # Oil
+        self.krw = self.__relperm(Sn=self.Swn, kr0=self.krw0, n=self.nw) # Water
+        self.kro = self.__relperm(Sn=1-self.Swn, kr0=self.kro0, n=self.no) # Oil
 
         # Fractional flow of water
         # fw = 1 / (1 + (kro/oil_viscosity) / (krw/water_viscosity))
@@ -67,7 +98,7 @@ class BL:
         self.RF_BT = self.N_BT / (1 - self.Swc) 
     
     # Relative permeability model
-    def relperm(self, Sn, kr0, n):
+    def __relperm(self, Sn, kr0, n):
         kr = kr0 * Sn ** n
         return kr
 
