757b174dd3024d82fa829ade3115de9d60f1a31d
mayuneko-re <49189086+mayuneko-re@users.noreply.github.com>
2019-10-08 15:05:17 +0900

    Update buckley_leverett.py

--- buckley_leverett.py
+++ buckley_leverett.py
@@ -37,7 +37,9 @@ class BL:
     """
     
     def __init__(self, muw, muo, Swc, Sor, krw0, kro0, nw, no, label=None):
-
+        """Initialize class
+        """
+        
         self.muw = muw # cP, Water viscosity
         self.muo = muo # cP, Oil viscosity
         self.Swc = Swc # Vol/Vol, Connate water saturation
@@ -49,11 +51,14 @@ class BL:
         self.label = label # Label for plot
 
     def calc(self):
-
+        """Calculate values by Buckley-Leverett Solution
+        """
+        
         # Water saturation and normalized water saturation
         self.Sw = np.linspace(start=self.Swc, stop=1-self.Sor, num = 300)
         self.Swn = (self.Sw-self.Swc) / (1-self.Swc-self.Sor)
 
+        # Relative permeability
         self.krw = self.__relperm(Sn=self.Swn, kr0=self.krw0, n=self.nw) # Water
         self.kro = self.__relperm(Sn=1-self.Swn, kr0=self.kro0, n=self.no) # Oil
 
@@ -64,7 +69,7 @@ class BL:
         # Wave velocity = Derivative of fractional flow of water
         self.vD = np.gradient(self.fw, self.Sw)
 
-        # Shock Front
+        # Determin Shock Front velocity and saturation
         slope_fw = (self.fw - 0) / (self.Sw - self.Swc)  # Warning: slope_fw[0] = nan
         self.vD_sf = np.nanmax(slope_fw) # Shock Front velocity
         self.Sw_sf = self.Sw[np.nanargmax(slope_fw)] # Shock Front water saturation
@@ -75,6 +80,7 @@ class BL:
         # Fractional flow of water with Shock Front
         self.fw_wSF = np.where(self.Sw<=self.Sw_sf, self.vD_sf*(self.Sw-self.Swc), self.fw)
 
+        # Dimensionless time array for plot
         self.tD = np.linspace(start=0, stop=2, num = 100)
         
         # Dimensionless breakthrough time
@@ -91,19 +97,23 @@ class BL:
         # Average water saturation in porous media
         self.Sw_ave = np.where(self.tD<=self.tD_BT, self.Swc+self.tD, self.Sw_out-(self.fw_out-1)/(1/self.tD))
 
+        # Oil production volume
         self.N = np.where(self.tD<=self.tD_BT, self.tD, self.Sw_ave-self.Swc)
-        self.N_BT = self.tD_BT
+        self.N_BT = self.tD_BT # at breakthrough
 
+        # Oil recovery factor
         self.RF = self.N / (1 - self.Swc)
-        self.RF_BT = self.N_BT / (1 - self.Swc) 
+        self.RF_BT = self.N_BT / (1 - self.Swc) # at breakthrough
     
-    # Relative permeability model
     def __relperm(self, Sn, kr0, n):
+        """Relative permeability model (Corey shape)
+        """
         kr = kr0 * Sn ** n
         return kr
 
-    # Get saturation profile at specific dimensionless time
     def profileSw(self, tD):
+        """Get saturation profile at specific dimensionless time
+        """
         xD = self.vD * tD
         xD = [1, *xD]
         Sw = [self.Swc, *self.Sw]
